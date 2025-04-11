7374d989b6db355161c1128c531067de8cb4d0ff
Adolfo Gante <adolfogante@gmail.com>
2025-04-10 23:47:33 -0700

    fixed dataset ids

--- benchmark_gui.py
+++ benchmark_gui.py
@@ -690,7 +690,18 @@ class BenchmarkQAIHub():
 					'dr9wmeyk2', 'dv910vwe2', 'd678rgpy2', 'd09y130m9', 'dd9ppqew9',
 				]
 
-				tflite_quantized = []
+				tflite_quantized = [
+					'dz7z4dmz9', 'dx9e8lok9', 'd67jwpq42', 'dw9v8gy57', 'd67ox08w7',
+					'dw2645359', 'dp7lg5yr2', 'dk7gkldg2', 'dr9wmkne2', 'dv9101zg2',
+					'do7mlynv9', 'd82ndnn17', 'd09y1ooe9', 'dr2qqkk82', 'dz7z4ddz9',
+					'd67jwpp42', 'dw2645559', 'dk7gkllg2', 'dz2r5kkp7', 'do7mlyyv9',
+					'd678rnnw2', 'dr2qqkm82', 'dn7xz6lz9', 'dj7d0zqy9', 'dd9pp8ro9',
+					'dw9v8gmm7', 'dp7lg5r42', 'dz2r5kvl7', 'dv74kpr02', 'dq9krqk87',
+					'do7mlyem9', 'dp70njld9', 'd82ndn6q7', 'dv951qjn2', 'dd9pp8zo9',
+					'dz7z4dy59', 'dx9e8lzo9', 'd67jwplp2', 'dw9v8gxm7', 'd693mxey7',
+					'dp7lg5k42', 'dr9wmkpw2', 'dv9101qn2', 'do7mlyqm9', 'd09y1o619',
+					'dd9pp8do9', 'dz7z4dl59', 'd693mxyy7', 'dk7gklvy2', 'dr9wmkgw2'
+				]
 
 				onnx_normal = [
 					'dj7d0qwq9', 'dz7z4jny9', 'dx9e85mv9', 'dw9v8m6z7', 'd67oxrvl7',
@@ -706,16 +717,16 @@ class BenchmarkQAIHub():
 				]
 
 				onnx_quantized = [
-					'dj7d0q5j9', 'dn7xzlpz9', 'dx9e85yk9', 'dk7gkn0g2', 'dr9wm6xe2',
-					'dv91038g2', 'dq9krgwv7', 'do7ml6wv9', 'd82ndr017', 'dn7xzl0z9',
-					'dj7d0qgy9', 'dz7z4jmz9', 'd67jwrq42', 'd67oxr8w7', 'd693mj887',
-					'dw264v359', 'dk7gkndg2', 'd82ndrn17', 'd09y1joe9', 'dv951zqw2',
-					'dr2qqmk82', 'dd9ppr8y9', 'dx9e85lk9', 'd67oxr0w7', 'dk7gknlg2',
-					'dr9wm6ke2', 'do7ml6yv9', 'd82ndrr17', 'd09y1jje9', 'dv951zzw2',
-					'dd9pprry9', 'dw9v8mm57', 'd693mjj87', 'dk7gknng2', 'dv74krrv2',
-					'dv91033g2', 'dq9krggv7', 'do7ml66v9', 'dp70nml59', 'dr2qqm682',
-					'dj7d0q3y9', 'dr2qqm6y2', 'dd9pprzo9', 'dz7z4jy59', 'dx9e85zo9',
-					'd67oxr667', 'dp7lgrk42', 'dk7gkngy2', 'dv9103qn2', 'do7ml6qm9'
+					'dp70njzd9', 'dz7z4d359', 'd67jwpmp2', 'd693mxvy7', 'dk7gklzy2',
+					'do7mly4m9', 'd82ndnvq7', 'dr2qqk0y2', 'dr2qqk0v2', 'dj7d0zpq9',
+					'd67ox0el7', 'dp7lg5412', 'dz2r5k1g7', 'dv9101gx2', 'do7mlygl9',
+					'd82ndnlo7', 'dr2qqklv2', 'dn7xz6369', 'dw9v8glz7', 'd693mxd07',
+					'dp7lg5612', 'dz2r5klg7', 'dv74kpnz2', 'dv91015x2', 'dq9krqvd7',
+					'd82ndnmo7', 'd09y1ozv9', 'dr2qqk4v2', 'dd9pp8nm9', 'dz7z4dny9',
+					'd67jwpgm2', 'dp7lg5v12', 'dz2r5keg7', 'dq9krq1d7', 'd678rnkm2',
+					'dp70njko9', 'dr2qqkev2', 'dj7d0z4q9', 'd67jwpzm2', 'dk7gklpe2',
+					'do7mlyzl9', 'd82ndn4o7', 'dv951qge2', 'dj7d0z8q9', 'd67jwpez2',
+					'd67oxrgg7', 'dw264vl69', 'dv74kr4y2', 'do7ml6139', 'dp70nm509'
 				]
 
 				file_type = self.optionmenu.get()
